31721c755676217a4e63f3645f20f581a5ff7cb0
YashwantNG <gawandeyashwant7@gmail.com>
2023-01-08 22:28:58 +0530

    image update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import Header from "./Header"
 import Meme from "./Meme"
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div>
       <Header />
